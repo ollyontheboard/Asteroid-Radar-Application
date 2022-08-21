7e961e478e2114b50427f2f527c67b7f47576c3e
Olisah Opiah <olisahhopiah@gmail.com>
2022-08-21 03:09:20 +0100

    Created Asteroid Table, Database and Dao

--- starter/app/src/main/java/com/udacity/asteroidradar/Asteroid.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/Asteroid.kt
@@ -1,10 +1,27 @@
 package com.udacity.asteroidradar
 
 import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class Asteroid(val id: Long, val codename: String, val closeApproachDate: String,
-                    val absoluteMagnitude: Double, val estimatedDiameter: Double,
-                    val relativeVelocity: Double, val distanceFromEarth: Double,
-                    val isPotentiallyHazardous: Boolean) : Parcelable
+@Entity(tableName = "asteroid_table")
+data class Asteroid(
+    @PrimaryKey
+    val id: Long,
+    @ColumnInfo(name="asteroid_codename")
+    val codename: String,
+    @ColumnInfo(name="asteroid_approach_date")
+    val closeApproachDate: String,
+    @ColumnInfo(name="asteroid_magnitude")
+    val absoluteMagnitude: Double,
+    @ColumnInfo(name="asteroid_diameter")
+    val estimatedDiameter: Double,
+    @ColumnInfo(name="asteroid_relative_velocity")
+    val relativeVelocity: Double,
+    @ColumnInfo(name="asteroid_codename")
+    val distanceFromEarth: Double,
+    @ColumnInfo(name="asteroid_is_hazardous")
+    val isPotentiallyHazardous: Boolean) : Parcelable
---
+++ starter/app/src/main/java/com/udacity/asteroidradar/database/AsteroidDatabase.kt
@@ -0,0 +1,29 @@
+package com.udacity.asteroidradar.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.udacity.asteroidradar.Asteroid
+
+@Database(entities = [Asteroid::class], version = 1, exportSchema = false)
+abstract class AsteroidDatabase : RoomDatabase() {
+
+    companion object{
+        @Volatile
+       private var INSTANCE : AsteroidDatabase? = null
+
+        fun getInstance(context: Context): AsteroidDatabase{
+            var instance = INSTANCE
+            if(instance==null){
+                instance = Room.databaseBuilder(context.applicationContext,
+                    AsteroidDatabase::class.java,"asteroid_database")
+                    .fallbackToDestructiveMigration()
+                    .build()
+                INSTANCE = instance
+            }
+            return instance
+        }
+
+    }
+}
