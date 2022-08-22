4273a2504d9fba408550801754aff1e32cd20327
Olisah Opiah <olisahhopiah@gmail.com>
2022-08-22 19:17:25 +0100

    Created retrofitservice and api. Getting json string from NEOWS

---
+++ starter/app/src/main/java/com/udacity/asteroidradar/api/NasaApiService.kt
@@ -0,0 +1,24 @@
+package com.udacity.asteroidradar.api
+
+import com.udacity.asteroidradar.Constants.BASE_URL
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
+interface NasaApiService {
+    @GET("neo/rest/v1/feed?start_date=2022-08-30&end_date=2022-08-31&api_key=HXmvjmeWkFttStMWa2UZ8boWKSEhVEYkbTttuHHV")
+    fun getAsteroids(): Call<String>
+
+}
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
+
+
+object NasaApi{
+    val retrofitservice : NasaApiService by lazy {
+        retrofit.create(NasaApiService::class.java)
+    }
+}
--- starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -1,6 +1,33 @@
 package com.udacity.asteroidradar.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.udacity.asteroidradar.api.NasaApi
+import retrofit2.Call
+import retrofit2.Response
+import javax.security.auth.callback.Callback
 
 class MainViewModel : ViewModel() {
+    private val _response = MutableLiveData<String>()
+    val response : LiveData<String>
+    get() = _response
+
+    init {
+        showAsteroids()
+    }
+
+    fun showAsteroids(){
+//call getasteroids method to make network request ans add enqueue method to run on background thread
+        //enqueue takes callback class as parameter which has methods to be called in success or failure
+        NasaApi.retrofitservice.getAsteroids().enqueue(object: retrofit2.Callback<String>{
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                _response.value = response.body()
+            }
+
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                _response.value = "Failure"+ t.message
+            }
+        })
+    }
 }
